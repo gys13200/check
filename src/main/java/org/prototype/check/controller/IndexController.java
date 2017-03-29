0caee582619d1edd64b24847072d7cb9cc58381a
gys <gys13200@163.com>
2017-03-30 00:03:56 +0800

    add UEditor

--- src/main/java/org/prototype/check/controller/IndexController.java
+++ src/main/java/org/prototype/check/controller/IndexController.java
@@ -1,8 +1,12 @@
 package org.prototype.check.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by gys on 2017/3/28.
@@ -12,7 +16,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class IndexController {
 
     @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @RequestMapping("/admin-index")
     public String index(){
         return "admin-index";
+    }
+
+    @RequestMapping(value = "/{path}", method = RequestMethod.GET)
+    public String hub(@PathVariable String path){
+
+        System.err.println("============ hub:" + path);
+
+        return path;
     }
 }
---
+++ src/main/java/org/prototype/check/controller/UEditorController.java
@@ -0,0 +1,38 @@
+package org.prototype.check.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import com.baidu.ueditor.ActionEnter;
+
+/**
+ * Created by gys on 2017/3/29.
+ */
+
+@Controller
+@RequestMapping("/ueditor")
+public class UEditorController {
+
+    @RequestMapping("/index")
+    public void ueditor(HttpServletRequest request, HttpServletResponse response) throws IOException{
+
+        try{
+            request.setCharacterEncoding( "utf-8" );
+            response.setHeader("Content-Type" , "text/html");
+            String realPath = request.getServletContext().getRealPath("/");
+            response.getWriter().write(new ActionEnter( request, realPath ).exec());
+
+        }catch (UnsupportedEncodingException e){
+            // do nothing
+        }
+    }
+
+    @RequestMapping("/uploadImage")
+    public void uploadImage(){
+
+    }
+}
